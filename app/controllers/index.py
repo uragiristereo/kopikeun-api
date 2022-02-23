3e46fc1b3f55c47f68dac92056a4cd9079057a37
Agung Watanabe <raava@outlook.co.id>
2022-02-23 14:23:56 +0700

    fix reset not working

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -11,14 +11,14 @@ def index():
 def reset():
     database_url = environ.get("DATABASE_URL")
 
-    if database_url == None:
-        db.drop_all()
-        db.create_all()
+    # if database_url == None:
+    db.drop_all()
+    db.create_all()
 
-        return {
-            "msg": "the database has been successfully reset"
-        }
-    else:
-        return {
-            "msg": "you're not allowed to do this operation"
-        }, 403
+    return {
+        "msg": "the database has been successfully reset"
+    }
+    # else:
+    #     return {
+    #         "msg": "you're not allowed to do this operation"
+    #     }, 403
